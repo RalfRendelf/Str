e5927531509c3616c545d6c65e16bca9b8fbb65b
RalfRendelf <vladislavungvicky@gmail.com>
2022-06-16 17:29:15 +0300

    Добавьте файлы проекта.

---
+++ Str/123.h
@@ -0,0 +1,185 @@
+#pragma once
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+void print(string s)
+{
+    cout << s << "\n";
+    system("pause");
+}
+void print(vector<list<string>> str)
+{
+    for (auto a : str) {
+        cout << '\t';
+        for (auto b : a)
+            cout << b;
+    }
+
+}
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs);
+private:
+    bool Proverka(string& word, string g);
+    string commonPart(vector<list<string>>& a);
+    
+};
+string Solution::longestCommonPrefix(vector<string>& strs) {
+    string str;
+    vector<list<string>> strH;
+    list<string> ah;
+    // strH.push_back(strH[0]);
+    
+    list<string>::iterator b;
+
+    int cnt = 0, cnt2 = 0;
+    for (auto k = 1; k < strs.size(); k++)
+    {
+
+        for (int h = 0; (h < strs[k].size()) && (h < strs[0].size()); h++)
+        {
+            if ((k > 1))
+            {
+               // print(strH);
+                cnt = 0;
+                if (strH.size() > 0)
+                {
+                    for (auto i = strH.begin(); i != strH.end(); i++) {
+                        b = (*i).begin();
+                        for (/*int  j = 0;j<strH[i].size(); j++*/auto& c : (*i))
+                        {
+
+                            if (!Proverka(strs[k], c))
+                            {
+                                if (cnt == 0)
+                                {
+                                   // print(c);
+                                    //print(strH);
+                                strH.erase(i);
+                              //  print(strH);
+                                if (strH.empty())
+                                {
+                                    str = "";
+                                        return str;
+                                }
+                                    break;
+                                }
+                                else
+                                {
+                                  //  print(strH);
+                                   // print(c);
+                                    (*i).erase(b,  (* i).end());
+                                  //  print(strH);
+                                    break;
+                                }
+                                
+
+                            }
+                            cnt++;
+                            b++;
+
+                        }
+                    }
+
+                }
+                else
+                {
+                    str = "";
+                    return str;
+                }
+
+
+            }
+            else {
+
+
+                if (!Proverka(strs[k], str + strs[0][h]))
+                {
+                    if (cnt > 0)
+                    {
+                       // print(str);
+                        strH[cnt2++].push_back(str);
+                        str = "";
+                        cnt = 0;
+                        strH.push_back(ah);
+                    }
+
+
+                }
+                else
+                {
+                    
+                    if (cnt > 0)
+                    {
+                      //  print(str);
+                        str += strs[0][h];
+                        strH[cnt2].push_back(str);
+                        cnt++;
+                    }
+                    else
+                    { 
+                       // print(str);
+                        strH.push_back(ah);
+                        str = strs[0][h];
+                        strH[cnt2].push_back(str);
+                        cnt++;
+                    }
+
+                }
+
+            }
+        }
+
+    }
+    if (!strH.empty())
+    {
+        cout << commonPart(strH);
+        return commonPart(strH);
+    }
+    else
+        return str = "";
+
+
+}
+bool Solution::Proverka(string& word, string g)
+{
+    int sh = 0;
+    for (int h = 0; (h /*+ g.size()*/) < word.size(); ++h)
+    {
+        for (int k = 0; (k < g.size())&&(h+g.size()<=word.size()); k++)
+        {
+            if (word[h+k] != g[k]) {
+                sh= 0;
+                break;
+            }
+            else {
+                ++sh;
+                if (g.size() == (k+1))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return sh;
+}
+string Solution::commonPart(vector<list<string>>& a)
+{
+    string str = "";
+    for (auto c : a)
+    {
+        for (auto v = c.begin(); v != c.end(); v++)
+        {
+            if (str < *v)
+            {
+                str = *v;
+            }
+        }
+    }
+    return str;
+}
